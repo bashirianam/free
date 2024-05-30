e254a8fd187fdb2a4eddbfcd2e7f1bf54fc0eeb7
bashirianam <169053740+bashirianam@users.noreply.github.com>
2024-05-30 22:57:05 +0330

    Update a.py

--- a.py
+++ a.py
@@ -20,7 +20,7 @@ def login():
 	    'x-app': 'tapswap_server',
 	    'x-cv': '608'
 	}
-	data = {"init_data":"query_id=AAG0MuMHAwAAALQy4wfmk7yO&user=%7B%22id%22%3A6574781108%2C%22first_name%22%3A%22Amir%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22fa%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717079211&hash=7f08085997b84f0f68d603134c071b610cb3f0b17d4fe86812f7d30f0681059d","referrer":"","bot_key":"app_bot_0"}
+	data = {"init_data":"query_id=AAG0MuMHAwAAALQy4wfVrgq3&user=%7B%22id%22%3A6574781108%2C%22first_name%22%3A%22Amir%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22fa%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1717083440&hash=f108015b041346a130018e94bbad5ed3215856c366f803f3094bdc08533a4e14","referrer":"","bot_key":"app_bot_0"}
 	
 	response = requests.post(url, headers=headers, json=data)
 	return response.json()["access_token"]
@@ -32,7 +32,7 @@ headers = {
 	    'Accept-Language': 'en-US,en;q=0.9',
 	    'Authorization': f'Bearer {login()}',
 	    'Connection': 'keep-alive',
-	    'Content-Id': '563036',
+	    'Content-Id': '6574231932',
 	    'Content-Type': 'application/json',
 	    'Origin': 'https://app.tapswap.club',
 	    'Referer': 'https://app.tapswap.club/',
